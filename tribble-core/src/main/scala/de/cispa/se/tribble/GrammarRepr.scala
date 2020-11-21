0f2fa70686adb57c779ef70d4008964a490e270c
Nikolas Havrikov <nikolas.havrikov@cispa.saarland>
2020-11-21 17:56:44 +0100

    Better error message on accessing absent grammar rule

--- tribble-core/src/main/scala/de/cispa/se/tribble/GrammarRepr.scala
+++ tribble-core/src/main/scala/de/cispa/se/tribble/GrammarRepr.scala
@@ -2,8 +2,9 @@ package de.cispa.se.tribble
 
 final case class GrammarRepr(start: NonTerminal, rules: Map[NonTerminal, DerivationRule]) {
   def root: DerivationRule = rules(start)
-  def get(reference: Reference): DerivationRule = rules(reference.name)
-  def get(nonTerminal: NonTerminal): DerivationRule = rules(nonTerminal)
+  def get(reference: Reference): DerivationRule = get(reference.name)
+  def get(nonTerminal: NonTerminal): DerivationRule = rules.getOrElse(nonTerminal,
+    throw new NoSuchElementException(s"The grammar has no rule named $nonTerminal"))
 
   /** @return the grammar created by adding the given production */
   private[tribble] def +(production: Production): GrammarRepr = {
