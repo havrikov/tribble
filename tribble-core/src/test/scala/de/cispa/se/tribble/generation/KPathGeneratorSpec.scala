b4082b593844c456965ffce0381cb553066f0b17
Alexander Kampmann <alexander.kampmann@cispa.saarland>
2021-03-01 11:57:03 +0100

    a test which showcases a missing kPath (found within the Alhazen test suite)
    (cherry picked from commit ad3bbe73c2f5ed42e1338d8bf5558ddd80497dc1)

--- tribble-core/src/test/scala/de/cispa/se/tribble/generation/KPathGeneratorSpec.scala
+++ tribble-core/src/test/scala/de/cispa/se/tribble/generation/KPathGeneratorSpec.scala
@@ -2,8 +2,9 @@ package de.cispa.se.tribble.generation
 
 import de.cispa.se.tribble.dsl._
 import de.cispa.se.tribble.input.SharedModelAssembler
-import de.cispa.se.tribble.{GrammarRepr, TestSpecification}
+import de.cispa.se.tribble.{Alternation, Concatenation, GrammarRepr, TestSpecification}
 
+import scala.List
 import scala.util.Random
 
 class KPathGeneratorSpec extends TestSpecification with SharedModelAssembler {
@@ -24,5 +25,24 @@ class KPathGeneratorSpec extends TestSpecification with SharedModelAssembler {
     val generated = generator.generateForest().take(1000).toList
 
     generated.size should be < 1000
+  }
+
+  "The KPathCoverageGoal" should "have [expr@0->expr@4->expr@4->expr@4 as a target" in {
+    val g = Grammar(
+      'start := 'expr,
+      'expr := "a" | 'expr ~ "+"~'expr
+    )
+    implicit val grammar: GrammarRepr = modelAssembler.assemble(g.productions)
+    implicit val random: Random = new Random(42)
+
+    val goal = new KPathCoverageGoal(4)
+
+    // construct the target path
+    val alt = grammar("expr").asInstanceOf[Alternation]
+    val concat = alt.alternatives(1).asInstanceOf[Concatenation]
+    val start = grammar.start
+    val expr4 = concat.elements.head
+
+    goal.targets should contain(List(start, expr4, expr4, expr4))
   }
 }
