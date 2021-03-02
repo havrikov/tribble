8f40538d6a35efd333eb2383ac685db5f9c97765
Nikolas Havrikov <nikolas.havrikov@cispa.saarland>
2021-03-02 16:40:04 +0100

    No need to keep the reference the the grammar in Reachability
    (cherry picked from commit 92d310144dd71c7152abe487c75d481c3265de20)

--- tribble-core/src/main/scala/de/cispa/se/tribble/generation/Reachability.scala
+++ tribble-core/src/main/scala/de/cispa/se/tribble/generation/Reachability.scala
@@ -12,7 +12,7 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable
 
 
-class Reachability(private val grammar: GrammarRepr) {
+class Reachability(grammar: GrammarRepr) {
   /**
     * This directed unweighted graph represents the derivations of the grammar.
     * The vertices are [[DerivationRule]]s and the edges are possible derivations.
