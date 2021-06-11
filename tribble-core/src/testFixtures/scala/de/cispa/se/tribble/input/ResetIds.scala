da0e0acdcb60e875a5127d290cd835869b6a7dd4
Mark Schuegraf <mark.schuegraf@posteo.de>
2021-06-11 20:56:35 +0200

    fixed bug where reset ids would not reset lazily instantiated elements
    (cherry picked from commit 3fe479ac73f0ca3ca54b7afa9e7cf8b0b2f17600)

--- tribble-core/src/testFixtures/scala/de/cispa/se/tribble/input/ResetIds.scala
+++ tribble-core/src/testFixtures/scala/de/cispa/se/tribble/input/ResetIds.scala
@@ -4,7 +4,7 @@ package input
 /** Resets the id of all derivation rules to [[DerivationRule.DEFAULT_ID]]. Useful only in testing. */
 private[input] object ResetIds extends AssemblyPhase {
   override def process(grammar: GrammarRepr): GrammarRepr = {
-    grammar.rules.values.flatMap(_.toStream).foreach(_.id = DerivationRule.DEFAULT_ID)
+    grammar.rules.values.flatMap(_.toStream).view.force.foreach(_.id = DerivationRule.DEFAULT_ID)
     grammar
   }
 }
